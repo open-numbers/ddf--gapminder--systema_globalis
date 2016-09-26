b6afe6383d6accb8298539efa10ccf9ca321b3c7
semio <prairy.long@gmail.com>
2016-09-26 15:00:49 +0800

    update output and some new files

---
+++ etl/scripts/etl.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from ddf_utils import chef
+import os
+
+out_dir = '../../'
+recipe_file = '../recipes/recipe_main.yaml'
+
+if __name__ == '__main__':
+    # removing old files
+    for f in os.listdir(out_dir):
+        if f.startswith("ddf--"):
+            os.remove(os.path.join(out_dir, f))
+
+    recipe = chef.build_recipe(recipe_file)
+    res = chef.run_recipe(recipe)
+    chef.dish_to_csv(res, out_dir)
