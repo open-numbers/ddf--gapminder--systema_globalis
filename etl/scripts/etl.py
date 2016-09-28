dee4ac8351b2de48d74e4731aea91b2d23bb3e05
semio <prairy.long@gmail.com>
2016-09-28 09:29:46 +0800

    script: add tag column

--- etl/scripts/etl.py
+++ etl/scripts/etl.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from ddf_utils import chef
+from ddf_utils.index import create_index_file
 import os
 
 out_dir = '../../'
@@ -15,3 +16,5 @@ if __name__ == '__main__':
     recipe = chef.build_recipe(recipe_file)
     res = chef.run_recipe(recipe)
     chef.dish_to_csv(res, out_dir)
+
+    create_index_file(out_dir)
---
+++ etl/scripts/patch.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import os
+
+out_dir = '../../'
+
+def concepts_tag_column():
+    """add tag column to measures"""
+    concs = pd.read_csv(os.path.join(out_dir, 'ddf--concepts.csv'))
+    graph = pd.read_excel('../source/graph_settings.xlsx', sheetname='Indicators')
+    mappin = pd.read_excel('../source/Gapminder world tag tree.xlsx', skip_footer=4)
+
+    measures = concs[concs['concept_type'] == 'measure']
+    measures = measures.set_index('concept').drop(['age', 'latitude', 'longitude'])
+
+    graph = graph.set_index('ddf_id')
+    m = graph.loc[measures.index, ['Menu level1', 'Menu level 2']].copy()
+    mappin = mappin.set_index(['tag_name'])
+
+    m2 = m.copy()
+
+    for k, v in m.iterrows():
+
+        if v['Menu level 2'] == 'Water' and v['Menu level1'] == 'Environment':
+            m2.loc[k, 'tag'] = 'environment_water'
+            continue
+
+        if v['Menu level 2'] == 'Water' and v['Menu level1'] == 'Infrastructure':
+            m2.loc[k, 'tag'] = 'infrastructure_water'
+            continue
+
+        if not pd.isnull(v['Menu level 2']):
+            m2.loc[k, 'tag'] = mappin.loc[v['Menu level 2'], 'tag_id']
+        elif not pd.isnull(v['Menu level1']):
+            m2.loc[k, 'tag'] = mappin.loc[v['Menu level1'], 'tag_id']
+        else:
+            continue
+
+
+    m2.loc['children_per_woman_total_fertility', 'tag'] = '_root, newborn_infants'
+    m2.loc['co2_emissions_tonnes_per_person', 'tag'] = '_root, emissions'
+    m2.loc['income_per_person_gdppercapita_ppp_inflation_adjusted', 'tag'] = '_root, incomes_growth'
+    m2.loc['child_mortality_0_5_year_olds_dying_per_1000_born', 'tag'] = '_root, mortality'
+    m2.loc['life_expectancy_years', 'tag'] = '_root, life_expectancy'
+
+    concs = concs.set_index('concept')
+    concs['tag'] = m2['tag']
+
+    concs.to_csv(os.path.join(out_dir, 'ddf--concepts.csv'))
