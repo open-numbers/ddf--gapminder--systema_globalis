704bc5d791edba07b23edbe48fcc1365b10a37a6
semio <prairy.long@gmail.com>
2016-12-21 10:46:10 +0800

    revert last commits on wdi
    because the history graph doesn't look correct

--- etl/scripts/etl.py
+++ etl/scripts/etl.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from ddf_utils import chef
-from ddf_utils.index import get_datapackage
+from ddf_utils.index import create_index_file
 import patch
 import os
-import json
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -%(levelname)s- %(message)s',
+logging.basicConfig(level=logging.INFO, format='%(asctime)s -%(levelname)s- %(message)s',
                     datefmt="%H:%M:%S"
                     )
 
@@ -22,14 +21,8 @@ if __name__ == '__main__':
 
     recipe = chef.build_recipe(recipe_file)
     res = chef.run_recipe(recipe)
-    print('saving result to disk...')
     chef.dish_to_csv(res, out_dir)
 
     patch.do_all_changes()
 
-    # TODO: keep older datapacakge's basic info(author etc)
-    datapackage = get_datapackage(out_dir)
-    with open(os.path.join(out_dir, 'datapackage.json'), 'w', encoding='utf8') as f:
-        json.dump(datapackage, f, indent=4, ensure_ascii=False)
-
-    print('Done.')
+    create_index_file(out_dir)
